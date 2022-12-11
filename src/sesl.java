7b5530779a8054dfdf4cd188bf5f4bc61d68ebf8
yusuftalha37 <109287557+yusuftalha37@users.noreply.github.com>
2022-12-11 19:05:47 +0300

    Initial commit

---
+++ src/sesl.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class sesl {
+    public static void main(String[] args) {
+        char harf= 'I';
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'U':
+            case 'O':
+                System.out.println("Kalın Sesli Harf");
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+
+
+
+
+        }
+    }
+}
